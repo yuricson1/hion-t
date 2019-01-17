04745042035ba7df83a9430ed79b2bf467a67c1d
y-kon <yuricson1@gmail.com>
2019-01-17 17:29:15 +0900

    test

---
+++ index.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>５５５エージェンシー</title>
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/slick-theme.css" rel="stylesheet" type="text/css">
+    <link href="css/slick.css" rel="stylesheet" type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script type="text/javascript" src="js/slick.min.js"></script>
+
+</head>
+
+<body id="hometop">
+
+<?php require_once($_SERVER["DOCUMENT_ROOT"].'/include/header.php');?>
+
+<div id="hometop">
+    <div class="center">
+        <ul>
+            <li>
+                <div class="rogoLift"><p>Support your dream</p></div>
+            </li>
+            <li>
+                <div class="rogo"><a href="/">
+                        <img src="imges/logo_w.png" alt="５５５エージェンシー"></a></div>
+            </li>
+
+
+            <li>
+                <div class="rogoRiht"><p>Catch your dream</p></div>
+            </li>
+        </ul>
+    </div>
+</div>
+<div id="home"></div>
+<div class="topImg">
+    <div class="topImg2">
+        <a href="/talent/detail/?talent_name=marina"><img src="imges/bnr_03.png" alt="MARINAメジャーデビュー">
+    </div>
+</div>
+
+
+<!-- <div id="topGallary"></div> -->
+
+
+<
+
+</body>
+</html>
